5b2063ffa91576420172c4fff553a2c92b76ed9d
tbreslein <tommy.breslein@protonmail.com>
2025-05-04 13:03:07 +0200

    fix/factor out system linux module

---
+++ modules/system/linux/default.nix
@@ -0,0 +1,71 @@
+{ pkgs-unstable, ... }: {
+  imports = [
+    # ./desktop
+  ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      timeout = 1;
+    };
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 8384 22000 ];
+      allowedUDPPorts = [ 22000 21027 ];
+    };
+  };
+
+  users.users.tommy = {
+    isNormalUser = true;
+    description = "tommy";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  environment = {
+    systemPackages = with pkgs-unstable; [
+      vim
+      git
+      neovim
+      gnumake
+      gcc
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  time.timeZone = "Europe/Berlin";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
+  };
+
+  services = {
+    syncthing.enable = true;
+    # openssh.enable = true;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  system.stateVersion = "24.11";
+}
