7ba9f070ebc39e6dfbaa7d8b5de942bf9e072003
tbreslein <tommy.breslein@protonmail.com>
2025-07-16 17:57:57 +0200

    feat(answer): blah

--- hosts/answer/configuration.nix
+++ hosts/answer/configuration.nix
@@ -42,6 +42,7 @@ in
     enable = true;
     brews = [
       "gcc"
+      "node"
       { name = "syncthing"; restart_service = "changed"; }
     ];
     taps = [
@@ -50,6 +51,7 @@ in
       "homebrew/homebrew-bundle"
     ];
     casks = [
+      "1password"
       "amethyst"
       "anki"
       "balenaetcher"
@@ -85,122 +87,109 @@ in
 
   programs = {
     _1password.enable = true;
-    _1password-gui.enable = true;
     man.enable = true;
   };
 
   services = {
     aerospace = {
-      enable = true;
-      start-at-login = true;
-      enable-normalization-flatten-containers = true;
-      enable-normalization-opposite-orientation-for-nested-containers = true;
-      accordion-padding = 30;
-      default-root-container-layout = "tiles";
-      default-root-container-orientation = "auto";
-      on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
-      automatically-unhide-macos-hidden-apps = true;
-      key-mapping.preset = "qwerty";
-      gaps = {
-        inner = {
-          horizontal = 4;
-          vertical = 4;
+      enable = false;
+      settings = {
+        start-at-login = true;
+        enable-normalization-flatten-containers = true;
+        enable-normalization-opposite-orientation-for-nested-containers = true;
+        default-root-container-layout = "tiles";
+        default-root-container-orientation = "auto";
+        on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
+        automatically-unhide-macos-hidden-apps = true;
+        key-mapping.preset = "qwerty";
+        gaps = {
+          inner = {
+            horizontal = 4;
+            vertical = 4;
+          };
+          outer = {
+            left = 2;
+            bottom = 2;
+            top = 2;
+            right = 2;
+          };
         };
-        outer = {
-          left = 2;
-          bottom = 2;
-          top = 2;
-          right = 2;
+        mode = {
+          main.binding = {
+            alt-slash = "layout tiles horizontal vertical";
+            alt-comma = "layout accordion horizontal vertical";
+
+            cmd-h = "focus left";
+            cmd-j = "focus down";
+            cmd-k = "focus up";
+            cmd-l = "focus right";
+            cmd-ctrl-h = "move left";
+            cmd-ctrl-j = "move down";
+            cmd-ctrl-k = "move up";
+            cmd-ctrl-l = "move right";
+            cmd-alt-i = "resize smart -50";
+            cmd-alt-o = "resize smart +50";
+            cmd-t = "workspace t";
+            cmd-s = "workspace s";
+            cmd-r = "workspace r";
+            cmd-a = "workspace a";
+            cmd-g = "workspace g";
+            cmd-ctrl-t = "move-node-to-workspace t";
+            cmd-ctrl-s = "move-node-to-workspace s";
+            cmd-ctrl-r = "move-node-to-workspace r";
+            cmd-ctrl-a = "move-node-to-workspace a";
+            cmd-ctrl-g = "move-node-to-workspace g";
+            cmd-tab = "workspace-back-and-forth";
+            cmd-shift-tab = "move-workspace-to-monitor --wrap-around next";
+
+            cmd-f = "fullscreen";
+            cmd-ctrl-semicolon = "mode service";
+          };
+          service.binding = {
+            esc = [ "reload-config" "mode main" ];
+            r = [ "flatten-workspace-tree" "mode main" ]; # reset layout
+            f = [ "layout floating tiling" "mode main" ]; # Toggle between floating and tiling layout
+            backspace = [ "close-all-windows-but-current" "mode main" ];
+
+            # sticky is not yet supported https://github.com/nikitabobko/AeroSpace/issues/2
+            # s = ["layout sticky tiling" "mode main"];
+
+            alt-shift-h = [ "join-with left" "mode main" ];
+            alt-shift-j = [ "join-with down" "mode main" ];
+            alt-shift-k = [ "join-with up" "mode main" ];
+            alt-shift-l = [ "join-with right" "mode main" ];
+          };
         };
+        on-window-detected = [
+          {
+            "if".app-id = "com.brave.Browser";
+            # if.app-id = 'com.brave.Browser'
+            run = "move-node-to-workspace 1";
+          }
+          {
+            "if".app-id = "app.zen-browser.zen";
+            run = "move-node-to-workspace 1";
+          }
+          {
+            "if".app-id = "app.zen-browser.zen";
+            "if".window-title-regex-substring = "Picture-in-Picture";
+            run = "layout floating";
+          }
+          {
+            # "if".app-id = "org.alacritty";
+            "if".app-id = "com.mitchellh.ghostty";
+            run = "move-node-to-workspace 2";
+          }
+          {
+            "if".app-id = "com.microsoft.teams2";
+            run = "move-node-to-workspace 3";
+          }
+          {
+            "if".app-id = "com.microsoft.Outlook";
+            run = "move-node-to-workspace 4";
+          }
+        ];
       };
-      mode = {
-        main.binding = {
-          # All possible keys:
-          # - Letters.        a, b, c, ..., z
-          # - Numbers.        0, 1, 2, ..., 9
-          # - Keypad numbers. keypad0, keypad1, keypad2, ..., keypad9
-          # - F-keys.         f1, f2, ..., f20
-          # - Special keys.   minus, equal, period, comma, slash, backslash, quote, semicolon, backtick,
-          #                   leftSquareBracket, rightSquareBracket, space, enter, esc, backspace, tab
-          # - Keypad special. keypadClear, keypadDecimalMark, keypadDivide, keypadEnter, keypadEqual,
-          #                   keypadMinus, keypadMultiply, keypadPlus
-          # - Arrows.         left, down, up, right
-          # All possible modifiers: cmd, alt, ctrl, shift
-          # All possible commands: https://nikitabobko.github.io/AeroSpace/commands
-
-          alt-slash = "layout tiles horizontal vertical";
-          alt-comma = "layout accordion horizontal vertical";
-
-          cmd-h = "focus left";
-          cmd-j = "focus down";
-          cmd-k = "focus up";
-          cmd-l = "focus right";
-          cmd-ctrl-h = "move left";
-          cmd-ctrl-j = "move down";
-          cmd-ctrl-k = "move up";
-          cmd-ctrl-l = "move right";
-          cmd-alt-i = "resize smart -50";
-          cmd-alt-o = "resize smart +50";
-          cmd-t = "workspace t";
-          cmd-s = "workspace s";
-          cmd-r = "workspace r";
-          cmd-a = "workspace a";
-          cmd-g = "workspace g";
-          cmd-ctrl-t = "move-node-to-workspace t";
-          cmd-ctrl-s = "move-node-to-workspace s";
-          cmd-ctrl-r = "move-node-to-workspace r";
-          cmd-ctrl-a = "move-node-to-workspace a";
-          cmd-ctrl-g = "move-node-to-workspace g";
-          cmd-tab = "workspace-back-and-forth";
-          cmd-shift-tab = "move-workspace-to-monitor --wrap-around next";
-
-          cmd-f = "fullscreen";
-          cmd-ctrl-semicolon = "mode service";
-        };
-        service.binding = {
-          esc = [ "reload-config" "mode main" ];
-          r = [ "flatten-workspace-tree" "mode main" ]; # reset layout
-          f = [ "layout floating tiling" "mode main" ]; # Toggle between floating and tiling layout
-          backspace = [ "close-all-windows-but-current" "mode main" ];
-
-          # sticky is not yet supported https://github.com/nikitabobko/AeroSpace/issues/2
-          # s = ["layout sticky tiling" "mode main"];
-
-          alt-shift-h = [ "join-with left" "mode main" ];
-          alt-shift-j = [ "join-with down" "mode main" ];
-          alt-shift-k = [ "join-with up" "mode main" ];
-          alt-shift-l = [ "join-with right" "mode main" ];
-        };
-      };
-      on-window-detected = [
-        {
-          "if".app-id = "com.brave.Browser";
-          # if.app-id = 'com.brave.Browser'
-          run = "move-node-to-workspace 1";
-        }
-        {
-          "if".app-id = "app.zen-browser.zen";
-          run = "move-node-to-workspace 1";
-        }
-        {
-          "if".app-id = "app.zen-browser.zen";
-          "if".window-title-regex-substring = "Picture-in-Picture";
-          run = "layout floating";
-        }
-        {
-          # "if".app-id = "org.alacritty";
-          "if".app-id = "com.mitchellh.ghostty";
-          run = "move-node-to-workspace 2";
-        }
-        {
-          "if".app-id = "com.microsoft.teams2";
-          run = "move-node-to-workspace 3";
-        }
-        {
-          "if".app-id = "com.microsoft.Outlook";
-          run = "move-node-to-workspace 4";
-        }
-      ];
     };
     jankyborders = {
       # TODO
