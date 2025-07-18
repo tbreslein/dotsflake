c65d2b949f574a4d2cdc0701d3dc3b8873470816
tbreslein <tommy.breslein@protonmail.com>
2025-07-18 07:53:42 +0200

    refac(darwin): remove twork alias

--- modules/home/darwin/default.nix
+++ modules/home/darwin/default.nix
@@ -10,23 +10,9 @@ in
 
   config = lib.mkIf cfg.enable {
     home.shellAliases.mocostate = "tail -10 /tmp/mococlient.err.log";
-    programs.bash = {
-      profileExtra = /* bash */ ''
-        [[ -f "/opt/homebrew/bin/brew" ]] && \
-          eval "$(/opt/homebrew/bin/brew shellenv)"
-      '';
-      bashrcExtra = /* bash */ ''
-        twork() {
-          if [ "$TMUX" != "" ]; then
-            if ! tmux has-session -t work; then
-              tmux new-session -ds "work" -c "${config.my-home.work-dir}"
-            fi
-          else
-            tmux new-session -ds "work" -c "${config.my-home.work-dir}"
-            tmux a -t "work"
-          fi
-        }
-      '';
-    };
+    programs.bash.profileExtra = /* bash */ ''
+      [[ -f "/opt/homebrew/bin/brew" ]] && \
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    '';
   };
 }
