49dfed0e541b1e0d2a94c84957dc0941e72eb2d7
tbreslein <tommy.breslein@protonmail.com>
2025-07-20 17:30:59 +0200

    feat(minimal-nvim): stubs for TTest and make

--- modules/home/code/minimal-nvim/lua/tvim/user_commands.lua
+++ modules/home/code/minimal-nvim/lua/tvim/user_commands.lua
@@ -1,5 +1,11 @@
 vim.api.nvim_create_user_command("TForm", function()
   print("TForm not defined")
 end, { nargs = 1, desc = "Format file" })
-
 vim.keymap.set("n", "<leader>;f", ":TForm %<cr>", { noremap = true, silent = true })
+
+vim.api.nvim_create_user_command("TTest", function()
+  print("TTest not defined")
+end, { nargs = 0, desc = "Test project" })
+vim.keymap.set("n", "<leader>;t", ":TTest<cr>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>;m", ":make<cr>", { noremap = true, silent = true })
